fcd0ebc2162693f4f6d3ebcae2c93a91f14b7e92
Diego Ortin <dieortin@gmail.com>
2014-03-24 22:40:58 +0100

    Added information to README.md, fixed crash when connecting to DB

--- server/require.js
+++ server/require.js
@@ -1,6 +1,11 @@
 //REQUIRE MODULES
 var http = require("http"),
-    qs = require('querystring');
+    qs = require('querystring'),
+    mysql = require('mysql');
 
 //EXPORT MODULES
 exports.http = http;
+exports.qs = qs;
+exports.mysql = mysql;
+
+//NOTE: Don't forget to add new required modules to export!
--- server/server.js
+++ server/server.js
@@ -8,10 +8,27 @@ log("Config imported", "init");
 log("Requiring modules...", "init");
 var required = require("./require.js");
 log("Modules required", "init");
+log("Connecting to database...", "init");
+var db = required.mysql.createConnection({ //NOTE: Don't forget to start MySQL DB before running server!
+  host     : config.db.host,
+  user     : config.db.user,
+  password : config.db.password
+});
+db.connect();
+log("Connected to database", "init");
 log("Starting http server...", "init");
 required.http.createServer(function (req, res) {
   if (req.method == "POST") {
-    
+    //TODO: Really process POST requests
+    res.end("GET request received and processed");
+  } else if (req.method == "GET") {
+    //TODO: Add way to send responses with appropiate headers
+    //TODO: Parse Jade
+    res.end("GET request received and processed");
+  } else {
+    res.writeHead(405);
+    res.end();
   }
 }).listen(config.urlParams.port, config.urlParams.url);
 log("http server started at " + config.urlParams.url + ":" + config.urlParams.port, "init");
+//FIXME: Unhandled error event!
